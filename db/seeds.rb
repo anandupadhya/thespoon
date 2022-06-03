503720b71f618f70020dee2eaf8575142eef7623
Anand Upadhya <anand.upadhya@yahoo.com>
2022-06-03 21:51:29 +0800

    Add restaurants controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+100.times do
+  p Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, rating: rand(1..5))
+end
